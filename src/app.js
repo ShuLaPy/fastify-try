656e8b6ad3ef620790c166760c7bc449b2fbda54
Shubham Lad <shubhamlad1001@gmail.com>
2022-03-23 23:46:42 +0530

    added models and connection to mongodb

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const fastify = require("fastify")({ logger: true });
 
+// db
+const mongoose = require("mongoose");
+
+// db connection
+mongoose
+  .connect("mongodb://localhost/fastify-try")
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error(err));
+
+// routes
 fastify.get("/", async (request, reply) => {
   return { visitor: "Shubham Lad" };
 });
 
+// starting server
 const start = async () => {
   try {
     await fastify.listen(3000);
